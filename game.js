4990c51579a9d93690c231bbe5ec78b5dcc26911
Samkit Barbhaya <samkitbarbhaya@gmail.com>
2023-03-01 16:20:00 -0700

    Added Button Event Listeners

--- game.js
+++ game.js
@@ -1,15 +1,17 @@
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
+
 function getComputerChoice(){
     const rps = ['Rock','Paper','Scissors'];
     let ind = getRandomInt(3);
     return rps[ind];
 }
+
 function playRound(computerSelection, playerSelection){
     playerSelection = playerSelection.toLowerCase();
     playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-    if(computerSelection==playerSelection) return "It's a tie!";
+    if(computerSelection==playerSelection) return "It's a tie! You both chose "+playerSelection;
     if((computerSelection=='Rock' && playerSelection=='Scissors') || 
         (computerSelection=='Paper' && playerSelection=='Rock') || 
         (computerSelection=='Scissors' && playerSelection=='Paper')){
@@ -18,11 +20,14 @@ function playRound(computerSelection, playerSelection){
         return "You Win! "+playerSelection+" beats "+computerSelection;
     }
 }
-function game(){
-    for(let i=0;i<5;i++){
-        const computerSelection = getComputerChoice();
-        const playerSelection = prompt();
-        console.log(playRound(computerSelection,playerSelection));
-    }
+
+function game(e){
+    const computerSelection = getComputerChoice();
+    const playerSelection = e.target.id;
+    console.log(playRound(computerSelection,playerSelection));
 }
-game();
+
+const buttons = document.querySelectorAll('.selection');
+buttons.forEach((button)=>{
+    button.addEventListener('click',game)
+});
